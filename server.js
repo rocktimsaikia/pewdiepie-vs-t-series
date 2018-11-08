739430174db7d1056bf03bfd4f115bee4d543597
RocktimSaikia <srocktim61@gmail.com>
2018-11-08 17:20:59 +0530

    transition property added to the body for smooth transition on the button toggle

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
   console.log(`Server is connected`);
